ee26a84daa937318135ac6aefd65016e4c82d743
Graham Keenan <graham.keenan@outlook.com>
2023-06-20 20:18:32 +0100

    feat: basic implementation of memcache

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::process::{Command, Stdio};
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
-    let store_dir = utils::create_safir_directory()?;
+    let store_dir = utils::create_safir_directory().await?;
     let mut safir = safir::Safir::init(&store_dir).await?;
 
     match &cli.command {
@@ -48,9 +48,21 @@ async fn main() -> std::io::Result<()> {
                     .spawn()
                     .expect("unable to spawn child process");
 
-                println!("Memcache started - {}", child.id());
+                let pid = child.id();
+                utils::write_pid_file(&store_dir, pid).await?;
+                println!("Memcache server started at 127.0.0.1:9876 - PID {}", pid);
             }
-            MemArgs::Stop => println!("Stopping mem server!"),
+            MemArgs::Stop => {
+                let pid = utils::load_pid(&store_dir).await?;
+                if let Err(err) = utils::kill_process(&store_dir, pid).await {
+                    eprintln!(
+                        "memcache server failed to stop, manual removal may be necessary - {}",
+                        err
+                    );
+                } else {
+                    println!("Stopping memcache server!");
+                }
+            }
         },
     }
     Ok(())
--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,70 @@
-use std::fs;
+// use std::fs;
 use std::io;
 use std::path::PathBuf;
+use tokio::fs;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-pub fn create_safir_directory() -> io::Result<PathBuf> {
+pub async fn create_safir_directory() -> io::Result<PathBuf> {
     let home_dir = dirs::home_dir().unwrap();
     let store_path = home_dir.join(".safirstore");
-    fs::create_dir_all(&store_path)?;
+    fs::create_dir_all(&store_path).await?;
 
     Ok(store_path)
+}
+
+pub async fn write_pid_file(store_dir: &PathBuf, pid: u32) -> io::Result<()> {
+    let fp = store_dir.join("rubin_server.pid");
+    let mut pid_file = fs::File::create(fp).await?;
+    pid_file.write_all(format!("{}", pid).as_bytes()).await?;
+
+    Ok(())
+}
+
+pub async fn load_pid(store_dir: &PathBuf) -> io::Result<u32> {
+    let fp = store_dir.join("rubin_server.pid");
+    let mut pid_file = fs::File::open(fp).await?;
+    let mut buf = vec![];
+
+    pid_file.read_to_end(&mut buf).await?;
+    let pid = String::from_utf8_lossy(&buf).parse::<u32>().unwrap();
+
+    Ok(pid)
+}
+
+pub async fn remove_pid_file(store_dir: &PathBuf) -> io::Result<()> {
+    let fp = store_dir.join("rubin_server.pid");
+    fs::remove_file(fp).await?;
+
+    Ok(())
+}
+
+#[cfg(target_family = "unix")]
+pub async fn kill_process(store_dir: &PathBuf, pid: u32) -> io::Result<()> {
+    if let Ok(process) = psutil::process::Process::new(pid) {
+        if let Err(err) = process.kill() {
+            eprintln!("failed to kill process: {}", err);
+        } else {
+            remove_pid_file(store_dir).await?;
+        }
+    } else {
+        eprintln!("failed to get process information");
+    }
+
+    Ok(())
+}
+
+#[cfg(target_os = "windows")]
+pub async fn kill_process(store_dir: &PathBuf, pid: u32) {
+    let output = Command::new("taskkill")
+        .arg("/F")
+        .arg("/PID")
+        .arg(pid.to_string())
+        .output()
+        .expect("failed to call taskkill");
+
+    if !output.status.success() {
+        eprintln!("failed to terminate process");
+    } else {
+        remove_pid_file(store_dir).await?;
+    }
 }
